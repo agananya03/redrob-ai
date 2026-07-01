8c163fadf45f587d85263e356190ce7e0a229dea
parvatisanthosh <parvathygsanthosh@gmail.com>
2026-07-01 11:22:30 +0530

    Fix SDK to Streamlit

--- check_st.py
+++
@@ -1,8 +0,0 @@
-﻿import subprocess
-import time
-p = subprocess.Popen(["streamlit", "run", "app.py"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-time.sleep(3)
-p.terminate()
-stdout, stderr = p.communicate()
-print("STDOUT:", stdout)
-print("STDERR:", stderr)
